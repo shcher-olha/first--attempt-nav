2dd813802ca6cdd6737759961f12fd1c92b66a27
shcher-olha <shcher.olha@gmail.com>
2024-04-22 19:45:49 +0200

    create Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Footer.css";
+import { FaInstagram } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-sm-6">
+          <h4>CONTACT US</h4>
+
+          <ul className="list">
+            <li className="item">
+              {" "}
+              <a
+                href="https://www.google.com/intl/ru/gmail/about/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdAlternateEmail className="icon" />
+              </a>
+              <span>sushi@gmail.com</span>
+            </li>
+            <li className="item">
+              <a
+                href="https://pics.wikireality.ru/upload/b/be/Instagram_Logo2016.png"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram className="icon" />
+              </a>
+              <span>our instagram</span>
+            </li>
+            <li className="item">
+              {" "}
+              <a
+                href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcResi1InzF4_FNunx9qnKEkdf-nwlke8mWhScv3zVtjEA&s"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTelegramPlane className="icon" />
+              </a>
+              <span>our Telegram</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-sm-6">
+          <h4>OUR LOCATION</h4>
+          <ul className="list">
+            <li className="item">
+              <a
+                href="https://planetofhotels.com/guide/sites/default/files/styles/paragraph__live_banner__lb_image__1880bp/public/live_banner/Vaduz-castle.jpeg"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaMapMarkerAlt className="icon" />
+              </a>
+              <span>Vaduz</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <div className="container">
       <p>Welcom to the world of Sushi</p>
 
-      <Link
-        to="/menu"
-        className="button"
-        
-      >
-        <button type="button" className="btn btn-light" style={{ fontSize: "30px" }}>
+      <Link to="/menu" className="button">
+        <button
+          type="button"
+          className="btn btn-light"
+          style={{ fontSize: "30px" }}
+        >
           Order Now
         </button>
       </Link>
 
-      
+      <Footer />
     </div>
   );
 }
